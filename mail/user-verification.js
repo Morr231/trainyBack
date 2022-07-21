fddc28f6fce59587f5beaef1b0d3a9da922df1bb
Almaz <63015341+Morr231@users.noreply.github.com>
2022-07-21 09:58:01 +0600

    trying to solve bugs 3

--- mail/user-verification.js
+++ mail/user-verification.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const sendMail = (text, email, emailSubject) => {
+    console.log(process.env.MY_EMAIL, process.env.MY_PASSWORD);
+
     const transporter = nodemailer.createTransport({
         service: "gmail",
         port: 465,
